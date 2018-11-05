cb60940725ae167aa5a19fc1c146ccb637d61dd7
Jakob Bellamy <jakbellamy@gmail.com>
2018-11-05 03:56:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,3 +10,14 @@ def batch_badge_creator(names_array)
     "Hello, my name is #{name}"
   end
 end
+
+def assign_rooms(names_array)
+  names.each_with_index.map do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(names_array)
+  batch_badge_creator(names_array)
+  assign_rooms(names_array)
+end
