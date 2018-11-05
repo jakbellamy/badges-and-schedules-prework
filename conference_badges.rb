2a3418c7ec0e4df38b916a8fef284fd4cabd570e
Jakob Bellamy <jakbellamy@gmail.com>
2018-11-05 03:52:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,12 @@
 # Write your code here.
+name_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array_of_names)
-  array_of_names.each do |name|
+def batch_badge_creator(names_array)
+  names_array.map do |name|
     "Hello, my name is #{name}"
   end
 end
